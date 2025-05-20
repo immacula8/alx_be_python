bab60d316291a193db3c17a4a3eb4ae6054ab1fc
Immaculate <geoffreyimmaculate@gmail.com>
2025-05-21 00:03:34 +0100

    Made a slight changes in the basic_operations code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,11 @@
 number1 = 10
 number2 = 5
 
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
+sum_numbers = number1 + number2
+subtract_numbers = number1 - number2
+multiply_numbers = number1 * number2
 
-print('Addition of ' + str(number1) + ' and ' + str(number2) + ' is ' + str(addition))
-print('Subtraction of ' + str(number1) + ' and ' + str(number2) + ' is ' + str(subtraction))
-print('Multiplication of ' + str(number1) + ' and ' + str(number2) + ' is ' + str(multiplication))
+print("Addition of", number1, "and", number2, "is", sum_numbers)
+print("Subtraction of", number1, "and", number2, "is", subtract_numbers)
+print("Multiplication of", number1, "and", number2, "is", multiply_numbers)
+
