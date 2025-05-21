3b7f2c067e0383eda27190fe3881946efd336f9a
Immaculate <geoffreyimmaculate@gmail.com>
2025-05-21 01:42:43 +0100

    Used a different code in simple_interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 principal = 1000
-rate = 0.05
+rate  = 0.05
 time = 3
 
 interest = principal * rate * time
 
-print('The simple interest is: ' + str(interest))
+print(f"The simple interest is: {interest}")
